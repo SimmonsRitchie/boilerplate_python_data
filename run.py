c7875b22cc444e4c7752365840354c100fd317d4
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-25 09:20:14 -0400

    integrate init call

--- run.py
+++ run.py
@@ -1,23 +1,11 @@
-import logging
-from datetime import datetime
-from definitions import DIR_DATA
-from logs.config.logging import logs_config
-from modules.helper.misc import delete_dir_contents
+from modules.init.init_program import init_program
 
 def main():
 
-    # init logging
-    scrape_start_time = datetime.now()
-    logs_config()
-    logging.info(f'Begin program run: {scrape_start_time}')
+    # init
+    init_program()
 
-    # create or clean download dir
-    if DIR_DATA.is_dir():
-        # delete files from previous run
-        delete_dir_contents(DIR_DATA)
-    else:
-        DIR_DATA.mkdir()
-
+    # actions to perform...
 
 
 
