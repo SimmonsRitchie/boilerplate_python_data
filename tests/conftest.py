01e5e69b7b4ada63ec8bc51752ac27c13f481c7e
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-08-07 13:59:10 -0400

    Add pytest stuff

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from dotenv import load_dotenv
+from typing import Dict
+from src.init.pandas_opts import pandas_opts
+
+
+def pytest_configure(config):
+    """
+    Allows plugins and conftest files to perform initial configuration.
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    # Makes it easier to see pandas DFs when printing to console
+    pandas_opts()
+    load_dotenv()
+
+
+@pytest.fixture(scope="session")
+def example_fixture() -> Dict[str, Dict]:
+    """
+    Example fixture.
+
+    """
+    return {
+        "id": "1",
+        "name": "example",
+    }
+
